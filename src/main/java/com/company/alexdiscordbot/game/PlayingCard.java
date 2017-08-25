3cb4a82bc4ca2811511fcad6489cb13bdf064818
Trubkin <trubkin.nikita@yandex.ru>
2017-08-25 20:59:32 +0300

    Update PlayingCard.java

--- src/main/java/com/company/alexdiscordbot/game/PlayingCard.java
+++ src/main/java/com/company/alexdiscordbot/game/PlayingCard.java
@@ -18,7 +18,7 @@ public class PlayingCard {
     }
 
     public enum Rank {
-        TWO("2", 2), THREE("3", 3), FOUR("4", 4), FIVE("5", 5), SIX("6", 6), SEVEN("7", 7), EIGHT("8", 8), NINE("9", 8), TEN("10", 10), JACK("J", 10), QUEEN("Q", 10), KING("K", 10), ACE("A", 11, 1);
+        TWO("2", 2), THREE("3", 3), FOUR("4", 4), FIVE("5", 5), SIX("6", 6), SEVEN("7", 7), EIGHT("8", 8), NINE("9", 9), TEN("10", 10), JACK("J", 10), QUEEN("Q", 10), KING("K", 10), ACE("A", 11, 1);
         private final String label;
         private final int value;
         private final int additionalValue;
